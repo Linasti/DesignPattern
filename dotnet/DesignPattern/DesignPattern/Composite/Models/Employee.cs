4a596d19dfb9f56eb3e610dd12d1b1466ef11919
Linasti <linasti.fr@gmail.com>
2020-07-22 19:36:46 +0200

    Add Composite pattern with TU

---
+++ dotnet/DesignPattern/DesignPattern/Composite/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Composite.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Salary { get; set; }
+
+        public string Dept { get; set; }
+
+        public List<Employee> Subordinates { get; set; }
+    }
+}
