6ed75c29d6f888ae2b8af9c70c8a6d523734ceb9
aitrigan <adrien.turpin@hotmail.fr>
2020-08-07 17:22:58 +0200

    Memento pattern

---
+++ dotnet/DesignPattern/DesignPattern.Test/Memento/MementoTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPattern.Memento;
+
+namespace DesignPattern.Test.Memento
+{
+    [TestFixture]
+    public class MementoTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Originator originator = new Originator();
+            CareTaker careTaker = new CareTaker();
+      
+            originator.SetState("State #1");
+            originator.SetState("State #2");
+            careTaker.Add(originator.SaveStateToMemento());
+      
+            originator.SetState("State #3");
+            careTaker.Add(originator.SaveStateToMemento());
+            originator.SetState("State #4");
+
+            Assert.AreEqual("State #4",originator.GetState());
+      
+            originator.GetStateFromMemento(careTaker.Get(0));
+            Assert.AreEqual("State #2", originator.GetState());
+
+            originator.GetStateFromMemento(careTaker.Get(1));
+            Assert.AreEqual("State #3", originator.GetState());
+        }
+    }
+}
---
+++ dotnet/DesignPattern/DesignPattern/Memento/Memento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Memento
+{
+    public class Memento
+    {
+        private readonly string _state;
+
+        public Memento(string state)
+        {
+            _state = state;
+        }
+
+        public string GetState()
+        {
+            return _state;
+        }
+    }
+}
